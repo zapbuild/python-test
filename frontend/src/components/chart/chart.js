6fb310b45630e2ccb08a4c4c9c147a0dfa1ea83a
Raman <ramanpreetsingh@zapbuild.com>
2021-05-22 09:32:49 +0530

    implemented Calender and Data modules

---
+++ frontend/src/components/chart/chart.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { CanvasJSChart } from 'canvasjs-react-charts';
+
+import chart  from "../../assets/data.json";
+
+let chartData = [];
+
+chart.data.plot_x.forEach((point, index, arr) => {
+  chartData.push({
+    x: chart.data.plot_x[index], 
+    y: chart.data.plot_y[index],
+    color: '#c3eb34',
+  })
+})
+
+class Chart extends Component {
+	constructor() {
+		super();
+		this.toggleDataSeries = this.toggleDataSeries.bind(this);
+	}
+	toggleDataSeries(e){
+		if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+			e.dataSeries.visible = false;
+		}
+		else{
+			e.dataSeries.visible = true;
+		}
+		this.chart.render();
+	}
+	render() {
+		const options = {
+      animationEnabled: true,
+      zoomEnabled: true,
+      width: 600,
+      height: 600,
+      title:{
+        text: "wall thickness plot"
+      },
+      axisX: {
+        title:"Position",
+        minimum: 0,
+        maximum: 70
+      },
+      axisY:{
+        title: "Elevation",
+        minimum: 0,
+        maximum: 70
+      },
+      data: [{
+        type: "scatter",
+        toolTipContent: "<b>X: </b>{x}<br/><b>Y: </b>{y}",
+        dataPoints: chartData
+      }]
+    }
+		return (
+		<div>
+			<CanvasJSChart options = {options}
+			/>
+		</div>
+		);
+	}
+}
+
+export default Chart;                            
